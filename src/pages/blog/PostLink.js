88ee571c2e12d56cd5e371aeadea129d7aca527e
Rushil Saraogi <rushilsaraogi@Rushils-MBP.fios-router.home>
2019-08-03 13:28:27 -0400

    loader for post link

--- src/pages/blog/PostLink.js
+++ src/pages/blog/PostLink.js
@@ -2,15 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from './styles/post-link.module.scss';
 
-const PostLink = ({ post }) => (
-  <Link className={`${styles.link} no-hover`} to={post.frontmatter.path}>
-    <div>
-      <img src={post.frontmatter.image.childImageSharp.fluid.src} />
-      <h2>{post.frontmatter.title}</h2>
-      <small>{post.frontmatter.date}</small>
-      <p className={styles.excerpt}>{post.excerpt}</p>
-    </div>
-  </Link>
-)
+const PostLink = ({ post }) => {
+  if (!post) return (
+    <p>Loading...</p>
+  )
+
+  return (
+    <Link className={`${styles.link} no-hover`} to={post.frontmatter.path}>
+      <div>
+        <img src={post.frontmatter.image.childImageSharp.fluid.src} />
+        <h2>{post.frontmatter.title}</h2>
+        <small>{post.frontmatter.date}</small>
+        <p className={styles.excerpt}>{post.excerpt}</p>
+      </div>
+    </Link>
+  )
+}
 
 export default PostLink
